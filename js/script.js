635fdd978c42ec8f316175366bce2b40a65abf20
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-08-24 16:17:01 +0200

    js: add second milestone

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 //*Creare l’array di oggetti con le informazioni fornite.
-
 const team =[
     {
         name:'Wayne Barnett',
@@ -43,3 +42,14 @@ const team =[
 for (let teamEl of team) {
     console.log('il nome è: ' + teamEl.name + ' il suo ruole è: ' +  teamEl.rule,' la sua foto è: ' + teamEl.photo);  
 }
+
+//* Stampare le stesse informazioni su DOM sottoforma di stringhe
+const containerEl = document.getElementById('container');
+
+let info = '';
+
+for (let teamEl of team) {
+    info+= `${teamEl.name} <br> ${teamEl.rule} <br> ${teamEl.photo}<br>`
+};
+
+containerEl.innerHTML = `${info}`;
